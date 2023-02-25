45242aa06693c34d4a65bc5b666cf53416415e1b
Dorian Terbah <dorian.terb@gmail.com>
2023-02-25 12:45:28 +0100

    This API contains now only vue component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,2 @@
 export { default as SketchColumnSelector } from './SketchColumnSelector.vue';
 export { default as SketchComponentModal } from './SketchComponentModal.vue';
-export { default as saveFile } from './file-saver';
-export { default as SketchComponent } from './sketch-component';
-export { default as SketchWrapper } from './sketch-wrapper';
-
-export { ComponentSlotConfiguration, ComponentConfiguration } from './component-configuration';
-export { Queue, Stack } from './data-structures';
-export { Class, GenericSketchComponentClass, opt} from './types';
-export { SketchComponentFactory } from './factory';
-
-
-export { ArrayQueue, ArrayStack } from './data-structures';
-export { HTTPService } from './service';
